9d5121fc51781842c2e97ad412498d960820732d
Tinku Kalita <tinku.kvs@gmail.com>
2024-06-29 20:43:33 +0530

    Handle GET/POST topics

--- barta/topic.go
+++ barta/topic.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+  "errors"
 )
 
 type Topic struct {
@@ -14,7 +15,7 @@ type Topic struct {
   ClusterAddr       string
 }
 
-func NewTopic(topicName string, clusterAddr string) *Topic {
+func NewTopic(topicName string, clusterAddr string) (*Topic, error) {
   t := &Topic{
     TopicName:  topicName,
     ClusterAddr: clusterAddr,
@@ -23,12 +24,47 @@ func NewTopic(topicName string, clusterAddr string) *Topic {
   stream, err := json.Marshal(t)
   if err != nil {
     log.Println("Unable to serialize topic")
-    return new(Topic)
+    return new(Topic), errors.New("Unable to serialize topic")
   }
-  _, err = http.Post(fmt.Sprintf("http://%s/topic/post", clusterAddr), "application/json", bytes.NewReader(stream))
+  r, err := http.Post(fmt.Sprintf("http://%s/topic/post", clusterAddr), "application/json", bytes.NewReader(stream))
   if err != nil {
-    log.Println(err.Error())
+    return new(Topic), err
   }
 
-  return t
+  if r.StatusCode != 201 {
+    log.Println(r.StatusCode)
+    return new(Topic), errors.New("Unable to create a topic")
+  }
+
+
+  return t, nil
+}
+
+func GetTopic(topicName string, clusterAddr string) (*Topic, error) {
+  t := &Topic{
+    TopicName:  topicName,
+    ClusterAddr: clusterAddr,
+  }
+
+  _, err := json.Marshal(t)
+  if err != nil {
+    log.Println("Unable to serialize topic")
+    return new(Topic), errors.New("Unable to serialize topic")
+  }
+  r, err := http.Get(fmt.Sprintf("http://%s/topic/get/%s", clusterAddr, topicName))
+  if err != nil {
+    return new(Topic), err
+  }
+
+  if r.StatusCode != 200 {
+    log.Println("Topic does not exist")
+    log.Println("Creating a new topic...")
+
+    return NewTopic(topicName, clusterAddr)
+  }
+
+  log.Println("Getting the topic")
+  json.NewDecoder(r.Body).Decode(&t)
+
+  return t, nil
 }
